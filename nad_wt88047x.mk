7362652da378914d240243825b1cd72168e1ab2a
xoo2001 <ekora2001@gmail.com>
2021-06-11 21:07:01 +0700

    wt88047x: ships aosp clock

--- nad_wt88047x.mk
+++ nad_wt88047x.mk
@@ -19,6 +19,7 @@ $(call inherit-product, device/xiaomi/wt88047x/full_wt88047x.mk)
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 USE_PIXEL_CHARGING := true
 TARGET_USES_BLUR := true
+USE_AOSP_CLOCK := true
 
 # Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
